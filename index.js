eca27f8a71358edfae7763bb334189998ba5bba8
Nam Giang <kyng@ece.ubc.ca>
2015-06-30 06:41:15 +0000

    http and https

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    fs = require('fs'),
+    app = express(),
+    https = require('https').createServer({key: fs.readFileSync('key.pem'),cert: fs.readFileSync('cert.pem')}, app),
+    http = require('http').createServer(app);
 
 app.set('port', (process.env.PORT || 5000));
+app.set('sport', (process.env.PORT || 443));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/index', function(request, response) {
     response.send('Hi World');
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+https.listen(app.get('sport'), function() {
+  console.log("Node app is running at :" + app.get('sport'));
+});
+
+http.listen(app.get('port'), function() {
+  console.log("Node app is running at :" + app.get('port'));
 });
